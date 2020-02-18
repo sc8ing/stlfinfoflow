d8fd83ff914de7a89bedfd4a5cc17b8cc62dae57
sc8ing <16541352+sc8ing@users.noreply.github.com>
2020-02-18 17:31:43 -0500

    copied and pruned, compiles

---
+++ Smallstep.v
@@ -0,0 +1,73 @@
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import Init.Nat.
+From Coq Require Import omega.Omega.
+From Coq Require Import Lists.List.
+Import ListNotations.
+From STLCIF Require Import Maps.
+
+(** * Relations *)
+
+Definition relation (X : Type) := X -> X -> Prop.
+
+Definition deterministic {X : Type} (R : relation X) :=
+  forall x y1 y2 : X, R x y1 -> R x y2 -> y1 = y2.
+
+(*
+Ltac solve_by_inverts n :=
+  match goal with | H : ?T |- _ =>
+  match type of T with Prop =>
+    solve [
+      inversion H;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
+
+Ltac solve_by_invert :=
+  solve_by_inverts 1.
+  *)
+
+Definition normal_form {X : Type} (R : relation X) (t : X) : Prop :=
+  ~ exists t', R t t'.
+
+Inductive multi {X : Type} (R : relation X) : relation X :=
+  | multi_refl : forall (x : X), multi R x x
+  | multi_step : forall (x y z : X),
+                    R x y ->
+                    multi R y z ->
+                    multi R x z.
+
+Theorem multi_R : forall (X : Type) (R : relation X) (x y : X),
+    R x y -> (multi R) x y.
+Proof.
+  intros X R x y H.
+  apply multi_step with y. apply H. apply multi_refl.
+Qed.
+
+Theorem multi_trans :
+  forall (X : Type) (R : relation X) (x y z : X),
+      multi R x y  ->
+      multi R y z ->
+      multi R x z.
+Proof.
+  intros X R x y z G H.
+  induction G.
+    - (* multi_refl *) assumption.
+    - (* multi_step *)
+      apply multi_step with y. assumption.
+      apply IHG. assumption.
+Qed.
+
+Definition normalizing {X : Type} (R : relation X) :=
+  forall t, exists t',
+    (multi R) t t' /\ normal_form R t'.
+
+(*
+Tactic Notation "print_goal" :=
+  match goal with |- ?x => idtac x end.
+
+Tactic Notation "normalize" :=
+  repeat (print_goal; eapply multi_step ;
+            [ (eauto 10; fail) | (instantiate; simpl)]);
+  apply multi_refl.
+*)
