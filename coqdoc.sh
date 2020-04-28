6f6a46bc654d839eeda0fb19eae928b4965a0973
sc8ing <16541352+sc8ing@users.noreply.github.com>
2020-04-28 16:10:05 -0400

    updates

--- coqdoc.sh
+++ coqdoc.sh
@@ -4,6 +4,6 @@ coqdoc Stlc.v --latex \
     --no-index \
     -d ./doc \
     --title 'Formally Proving Validity for Type-Based Information Flow' \
-    -toc \
+    --table-of-contents \
     --preamble '\usepackage{bussproofs} \usepackage{amssymb} \usepackage{latexsym} \def\fCenter{{\mbox{\Large$\rightarrow$}}}' \
     -g # hide proofs by default
