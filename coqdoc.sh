398e3faa07b757275c148e2fcebab186fe860052
sc8ing <16541352+sc8ing@users.noreply.github.com>
2020-04-29 18:28:49 -0400

    sent draft to joe

--- coqdoc.sh
+++ coqdoc.sh
@@ -7,5 +7,30 @@ coqdoc Stlc.v --latex \
     -d ./doc \
     --title 'Formally Proving Validity for Type-Based Information Flow' \
     --table-of-contents \
-    --preamble '\usepackage{bussproofs} \usepackage{amssymb} \usepackage{latexsym} \def\fCenter{{\mbox{\Large$\rightarrow$}}}' \
+    --preamble '\usepackage{bussproofs} \usepackage{amssymb} \usepackage{latexsym} \def\fCenter{{\mbox{\Large$\rightarrow$}}} \usepackage{titlesec} \usepackage{lipsum} \usepackage{setspace}' \
     -g # hide proofs by default
+
+# \tableofcontents
+
+# \titleformat{\chapter}[display]
+#   {\normalfont\bfseries}{}{0pt}{\Huge}
+# 
+# \begin{titlepage}
+# \begin{center}
+# 
+# \begin{spacing}{1.5}
+#     \vspace*{\fill}
+# \end{spacing}
+# 
+# \begin{spacing}{2.5}
+#     \textbf{\Large A Machine-verified Proof for Information Flow Typing}\\[0.5cm]
+#     \vspace*{\fill}
+# \end{spacing}
+# 
+# \begin{spacing}{1.15}
+#     \textbf{\large Jacob Bennett}
+#     \date{}
+# \end{spacing}
+# 
+# \end{center}
+# \end{titlepage}
